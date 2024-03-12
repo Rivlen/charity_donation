6f39867163da5df1181b988fd3f7489cda847224
Rivlen <x.stawecki@gmail.com>
2024-03-12 11:31:02 +0100

    Add userbase

--- charity_donation/settings.py
+++ charity_donation/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "main.apps.MainConfig",
+    "userbase.apps.UserbaseConfig"
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "userbase.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
